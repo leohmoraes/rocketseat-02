929cbcd9398960a6d21c7fb5db3df686575fa9ed
Leo Moraes <moraes.leo@gmail.com>
2019-08-23 23:56:39 -0300

    Modulo 02 - Video 07 - Configurando o sequelize https://gist.github.com/leohmoraes/071945d8b6f8165e3c643dfa31559dec/

--- src/app.js
+++ src/app.js
@@ -9,19 +9,16 @@ import routes from './routes';
 
 class App {
   constructor() {
-    console.log('iniciado');
     this.server = express();
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
-    console.log('middlewares');
     this.server.use(express.json());
   }
 
   routes() {
-    console.log('routes');
     this.server.use(routes);
   }
 }
