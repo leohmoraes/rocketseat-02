484e6b88846809e0fe55a0d8c1c5a835a19e9d86
Leo Moraes <moraes.leo@gmail.com>
2019-09-01 09:02:55 -0300

    video 28 10 configurando o mongodb Yarn add mongoose docker run --name mongobarber -p 27017:27017 -d -t mongo
    REGISTROS DO PROCESSO
    https://github.com/leohmoraes/rocketseat-02/issues/5

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 // video 10 - criando a conexao com o BD e loader de models
 
 import Sequelize from 'sequelize';
-
+import mongoose from 'mongoose'; // video 28 10 configurando o mongodb
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment'; // Video 22 04 - migration e model de agendamento
@@ -13,6 +13,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo(); // video 28 10 configurando o mongodb
   }
 
   // conexao com BD e carregar modulos
@@ -22,6 +23,17 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    // video 28 10 configurando o mongodb
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+      }
+    );
   }
 }
 
